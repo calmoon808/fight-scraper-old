265e175879174517fe5e2b78660a25c90c80f5f5
cmoon <calmoon808@gmail.com>
2022-03-27 22:05:14 -1000

    Cleaned up old scraper and moved it to a new file

--- web_scraper.py
+++ web_scraper.py
@@ -2,30 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import os
+import time
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_experimental_option("detach", True)
 
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver.get("https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=https://discord-stock-moonbot.herokuapp.com/&client_id=C7XQAIY9SREZRXKWVTXMLWF9TMHB3ZP0%40AMER.OAUTHAP")
 # print(driver.page_source)
-print(driver.current_url);
-wait = WebDriverWait(driver, 10)
+driver.get("https://www.ufc.com/events")
+time.sleep(1);
 
-usernameInput = driver.find_element(By.ID, "username0");
-usernameInput.send_keys("cmoon808")
-passwordInput = driver.find_element(By.ID, "password1");
-passwordInput.send_keys("ElGaucho08!")
-submitButton = driver.find_element(By.ID, "accept");
-submitButton.click();
-submitButton = driver.find_element(By.ID, "accept");
-submitButton.click();
+# cookieNotificationElements = driver.find_element(By.CLASS_NAME, "onetrust-close-btn-handler onetrust-close-btn-ui banner-close-button mobile close-icon");
+buttons = driver.find_elements(By.CSS_SELECTOR, '[aria-label="Close Banner Button"]');
+buttons[len(buttons) - 1].click();
+time.sleep(1);
+
+viewFightCardButtons = driver.find_elements(By.CSS_SELECTOR, "[tabindex='0']");
+
+viewFightCardButtons[0].click();
+
+# print(driver.current_url);
+# wait.until(EC.url_changes(driver.current_url))
 
 print(driver.current_url);
+# driver.quit()
 print("Finished!")
