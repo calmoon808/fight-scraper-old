2e4531914c894c4476aa0a3e3e26733170ec9dd8
cmoon <calmoon808@gmail.com>
2022-03-27 16:42:54 -1000

    package update for heroku warning

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 
 chrome_options = webdriver.ChromeOptions()
@@ -6,7 +8,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.get("https://medium.com")
 print(driver.page_source)
 
