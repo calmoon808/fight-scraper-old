4ad6fb79615b728301b9e6b79b783acb839fb4d0
cmoon <calmoon808@gmail.com>
2022-03-28 20:36:54 -1000

    heroku debug

--- web_scraper.py
+++ web_scraper.py
@@ -10,7 +10,7 @@ import os
 import time
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_experimental_option("detach", True)
